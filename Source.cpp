efde93fad92bb14af5070f63fc1a0a277b425b77
Hєavenly <73426193+Heaveenlyy@users.noreply.github.com>
2020-11-30 16:48:49 +0200

    Lab5

---
+++ Source.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include "Header.h"
+
+using namespace std;
+
+
+
+
+
+
+	int main() 
+	{
+
+	Planet neptun = Planet();
+	neptun.name = "Neptun";
+	neptun.mass = 59000;
+	neptun.length_of_day = 50 ;
+	neptun.distance_from_sun = 4;
+	neptun.type = Terrestrial;
+	neptun.type = Jovian;
+
+	Planet saturn;
+	saturn.name = "Saturn";
+	saturn.mass = 25000;
+	saturn.length_of_day = 75;
+	saturn.distance_from_sun = 1;
+	saturn.type = Terrestrial;
+	saturn.type = Jovian;
+
+	Planets2 planets;
+
+	planets.addPlanet(neptun);
+	planets.addPlanet(saturn);
+
+	planets.sort();
+	planets.getinfo("Neptun");
+
+	planets.sort();
+	planets.getinfo("Saturn");
+
+    Distance(neptun, saturn);
+    Mass(neptun, saturn);
+
+	return 0;
+	}
